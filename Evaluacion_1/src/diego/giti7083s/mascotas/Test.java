89d5f8033ba5e5a92c3eea538a988b801ef022f0
JuarezDiego <diegos_italia@hotmail.com>
2016-02-05 08:13:46 -0600

    actualizado

---
+++ Evaluacion_1/src/diego/giti7083s/mascotas/Test.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package diego.giti7083s.mascotas;
+
+/**
+ * @author Diego
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
